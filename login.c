6ef4cdcdd1d8d2b073c88e8381baa35d962b3a31
Ali-Mv <ali.mv3134@gmail.com>
2026-02-11 21:50:26 +0330

    دیباگ لاگین

--- login.c
+++ login.c
@@ -1,9 +1,4 @@
-﻿#include "login.h"
-#include "defines.h"
-#include <stdio.h>
-#include <conio.h>
-#include <windows.h>
-#include <string.h>
+﻿#include "defines.h"
 
 User currentUser;
 
@@ -62,7 +57,6 @@ int AskPlayerLogin() {
     printf("\nChoose option (1-3): ");
 
     while (scanf("%d", &choice) != 1 || (choice < 1 || choice > 3)) {
-        while (getchar() != '\n');
         printf("Invalid input. Try again: ");
     }
 
@@ -70,116 +64,131 @@ int AskPlayerLogin() {
 }
 
 void SignUp() {
-    system("cls");
 
-    User newUser;
-    User tempUser;
-    FILE* file;
-    int exists = 0;
-    char confirmPass[MAX_PASSWORD_LEN];
+    while (1) {
+        system("cls");
+        User newUser;
+        User tempUser;
+        FILE* file;
+        int exists = 0;
+        char confirmPass[MAX_PASSWORD_LEN];
 
-    printf("================ Sign Up ================\n");
+        printf("================ Sign Up ================\n");
 
-    printf("Enter Username: ");
-    scanf("%s", newUser.username);
+        printf("Enter Username(max 20): ");
+        scanf("%20s", newUser.username);
 
-    file = fopen(USER_FILE, "rb");
-    if (file != NULL) {
+        file = fopen(USER_FILE, "rb");
+        if (file != NULL) {
+            while (fread(&tempUser, sizeof(User), 1, file)) {
+                if (strcmp(tempUser.username, newUser.username) == 0) {
+                    exists = 1;
+                    break;
+                }
+            }
+            fclose(file);
+        }
+
+        if (exists) {
+            printf("\nError: Username already exists!\n");
+            printf("Press any key to try again");
+            _getch();
+            continue;
+        }
+
+        while (1) {
+            system("cls");
+            printf("Enter Password (min 8 , max 20): ");
+            getPasswordMasked(newUser.password);
+
+            if (strlen(newUser.password) < 8) {
+                printf("Error: Password must be at least 8 characters long!\n");
+                printf("Press any key to try again");
+                _getch();
+                continue;
+            }
+
+            printf("Confirm Password: ");
+            getPasswordMasked(confirmPass);
+
+            if (strcmp(newUser.password, confirmPass) != 0) {
+                printf("Error: Passwords do not match! Try again.\n");
+                printf("Press any key to try again");
+                _getch();
+            }
+            else {
+                break;
+            }
+        }
+
+        newUser.score = 0;
+        newUser.gold = 0;
+        newUser.max_level = 1;
+
+        file = fopen(USER_FILE, "ab");
+        if (file == NULL) {
+            printf("\nError saving data!\n");
+            printf("Press any key to try again");
+            _getch();
+            continue;
+        }
+
+        fwrite(&newUser, sizeof(User), 1, file);
+        fclose(file);
+
+        printf("\nSign Up Successful! Please Login now.\n");
+        printf("Press any key to continue...");
+        _getch();
+        break;
+    }
+}
+
+void Login() {
+    while (1) {
+
+        system("cls");
+
+        char inputUser[MAX_USERNAME_LEN];
+        char inputPass[MAX_PASSWORD_LEN];
+        User tempUser;
+        FILE* file;
+        int found = 0;
+
+        printf("================ Login ================\n");
+        printf("Enter Username: ");
+        scanf("%s", inputUser);
+
+        printf("Enter Password: ");
+        getPasswordMasked(inputPass);
+
+        file = fopen(USER_FILE, "rb");
+        if (file == NULL) {
+            printf("\nNo users registered yet!\n");
+            printf("Press any key to return...");
+            _getch();
+            continue;
+        }
+
         while (fread(&tempUser, sizeof(User), 1, file)) {
-            if (strcmp(tempUser.username, newUser.username) == 0) {
-                exists = 1;
+            if (strcmp(tempUser.username, inputUser) == 0 &&
+                strcmp(tempUser.password, inputPass) == 0) {
+                found = 1;
                 break;
             }
         }
         fclose(file);
-    }
 
-    if (exists) {
-        printf("\nError: Username already exists!\n");
-        printf("Press any key to return...");
-        _getch();
-        return;
-    }
-
-    while (1) {
-        printf("Enter Password (min 8 chars): ");
-        getPasswordMasked(newUser.password);
-
-        if (strlen(newUser.password) < 8) {
-            printf("Error: Password must be at least 8 characters long!\n");
-            continue;
-        }
-
-        printf("Confirm Password: ");
-        getPasswordMasked(confirmPass);
-
-        if (strcmp(newUser.password, confirmPass) != 0) {
-            printf("Error: Passwords do not match! Try again.\n");
+        if (found) {
+            currentUser = tempUser;
+            printf("\nLogin Successful! Welcome %s\n", tempUser.username);
+            Sleep(1000);
         }
         else {
-            break;
+            printf("\nError: Invalid Username or Password.\n");
+            printf("Press any key to try again...");
+            _getch();
         }
+        break;
     }
-
-    newUser.score = 0;
-    newUser.gold = 0;
-    newUser.max_level = 1;
-
-    file = fopen(USER_FILE, "ab");
-    if (file == NULL) {
-        printf("\nError saving data!\n");
-        return;
-    }
-
-    fwrite(&newUser, sizeof(User), 1, file);
-    fclose(file);
-
-    printf("\nSign Up Successful! Please Login now.\n");
-    printf("Press any key to continue...");
-    _getch();
-}
-
-void Login() {
-    system("cls");
-
-    char inputUser[MAX_USERNAME_LEN];
-    char inputPass[MAX_PASSWORD_LEN];
-    User tempUser;
-    FILE* file;
-    int found = 0;
-
-    printf("================ Login ================\n");
-    printf("Enter Username: ");
-    scanf("%s", inputUser);
-
-    printf("Enter Password: ");
-    getPasswordMasked(inputPass);
-
-    file = fopen(USER_FILE, "rb");
-    if (file == NULL) {
-        printf("\nNo users registered yet!\n");
-        printf("Press any key to return...");
-        _getch();
-        return;
-    }
-
-    while (fread(&tempUser, sizeof(User), 1, file)) {
-        if (strcmp(tempUser.username, inputUser) == 0 &&
-            strcmp(tempUser.password, inputPass) == 0) {
-            found = 1;
-            break;
-        }
-    }
-    fclose(file);
-
-    if (found) {
-        currentUser = tempUser;
-        printf("\nLogin Successful! Welcome %s\n", tempUser.username);
-        Sleep(1000);
-    }
-    else {
-        printf("\nError: Invalid Username or Password.\n");
-        printf("Press any key to try again...");
-        _getch();
-    }
+    
 }
